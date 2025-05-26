e1802dd015ed92d5ac7dec609d8b6511e51a5a19
djumanov <djumanovdev@gmail.com>
2025-05-26 09:19:31 +0500

    2-mashq najarildi

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -1,1 +1,21 @@
+from decimal import Decimal
+from num2words import num2words
+
+food01=Decimal(input("Food Price: "))
+food02 = Decimal(input("Food Price: "))
+food03 = Decimal(input("Food Price: "))
+
+price = round(food01 + food02 + food03)
+words_en = num2words(price, to='currency', currency='USD')
+words_ru = num2words(price, lang='ru', to='currency', currency='USD')
+
+print(
+    "$",
+    price,
+    "(",
+    words_en,
+    ",",
+    words_ru,
+    ")"
+)
 
